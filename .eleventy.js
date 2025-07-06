cb64ee4036c7e91b0e62ca829ea204043f3ffd5e
Christian Noss <christian.noss@th-koeln.de>
2025-07-06 09:27:55 +0200

    Changed integration of editions

--- .eleventy.js
+++ .eleventy.js
@@ -15,11 +15,11 @@ const config = {
   "dist": "./docs",
   "compiledContent": "./compiled-content",
   "graphicPrefix": "GWN_",
-  "onlyDevObjects": false,
+  "onlyDevObjects": true,
   "generateLiterature": true,
   "generateAuthors": false,
-  "generatePaintings": true,
-  "generateArchivals": true,
+  "generatePaintings": false,
+  "generateArchivals": false,
   "generateDrawings": true,
   "generateGraphicsVirtualObjects": true,
   "pathPrefix": {
@@ -238,7 +238,7 @@ const appendToFile = (path, str) => {
 
 const getDrawingsCollection = (lang) => {
   const drawingsForLang = drawingsData[lang];
-  const devObjects = ['Z_US_NGA_2006-111-2', 'Z_US_MFAH_44-547'];
+  const devObjects = ['Z_DE_GNMN_Hz56'];
 
   const drawings = config.onlyDevObjects === true
     ? drawingsForLang.items.filter(item => devObjects.includes(item.inventoryNumber))
@@ -372,7 +372,7 @@ const getGraphicsRealObjectsCollection = (lang) => {
 
 const getGraphicsVirtualObjectsCollection = (lang) => {
   const graphicsVirtualObjectsForLang = graphicsVirtualObjectData[lang];
-  const devObjects = ["LC_HVI-57_80", "LC_HVI-19-21_16", "LC_HVI-68_92", "LC_HVI-56_79"]; // , "ANO_H-NONE-022", "LC_HVI-9_8", "LC_HVI-19-21_18","MIB_H-NONE-001", "MIB_H-NONE-002"
+  const devObjects = ["LC_HVI-5_4","LC_HVI-12_3","LC_HVI-19-21_10","LC_HVI-22_24","LC_HVI-24_26","LC_HVI-29_30e", "LC_HVI-56_79"]; // , "ANO_H-NONE-022", "LC_HVI-9_8", "LC_HVI-19-21_18","MIB_H-NONE-001", "MIB_H-NONE-002",  "LC_HVI-57_80", "LC_HVI-19-21_16", "LC_HVI-68_92", "LC_HVI-56_79"
   
   const graphicsVirtualObjects = config.onlyDevObjects === true 
     ? graphicsVirtualObjectsForLang.items.filter(item => devObjects.includes(item.inventoryNumber))
