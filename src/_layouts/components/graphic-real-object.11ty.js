c3a3bb552056c2d8e9567217854b7cf500f52f9b
Christian Noss <christian.noss@th-koeln.de>
2026-06-03 14:12:51 +0200

    Ändert die Umgebungsvariablen von METADATA_API_KEY zu API_KEY in mehreren Dateien

--- src/_layouts/components/graphic-real-object.11ty.js
+++ src/_layouts/components/graphic-real-object.11ty.js
@@ -116,7 +116,7 @@ exports.getRealObject = function (eleventy, pageData, langCode, masterData) {
       <script>
         const objectData = {};
         objectData.g = "${process.env.API_METADATA_EXIF_ENDPOINT}";
-        objectData.metadataApiKey = "${shouldIncludeMetadataEditor ? process.env.METADATA_API_KEY : ''}";
+        objectData.metadataApiKey = "${process.env.API_KEY}";
         objectData.langCode = "${langCode}";
         objectData.imageStack = ${imageStack};
         objectData.baseUrl = "${baseUrl}/${langCode}";
